b7eada76caf3539204046fb983f918a5f604c30b
MozahidNahin <mozahidulislamnahin@gmail.com>
2024-10-23 23:20:43 +0600

    made nav bar and blog section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
+import Header from './components/Header/Header'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-6xl bg-red-100'>Knowledge Cafe</h1>
-      
-      
+      <Header></Header>
+      <div className='md:flex '>
+      <Blogs></Blogs>
+      <Bookmarks></Bookmarks>
+      </div>
     </>
   )
 }
